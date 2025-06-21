b7e77099c4b3f83b780b6f3f0dc656f30bb6211d
ELMEHDIFATHI <mehdiblabo@gmail.com>
2025-06-21 17:04:26 +0100

    creating crud api for Account And handling validation and exception error

--- accounts/src/main/java/com/hp/accounts/Controller/AccountsController.java
+++ accounts/src/main/java/com/hp/accounts/Controller/AccountsController.java
@@ -5,6 +5,8 @@ import com.hp.accounts.Constants.AccountsConstant;
 import com.hp.accounts.Dto.CustomerDto;
 import com.hp.accounts.Dto.ResponseDto;
 import com.hp.accounts.Services.IAccountService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -26,10 +28,48 @@ public class AccountsController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto){
         accountService.createAccount(customerDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ResponseDto(AccountsConstant.STATUS_201,AccountsConstant.MESSAGE_201));
+    }
+
+    @GetMapping("/fetch")
+    public ResponseEntity<CustomerDto> fetchAccount(@RequestParam
+                                                        @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+                                                        String mobileNumber){
+        CustomerDto customerDto = accountService.fetchAccount(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(customerDto);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> UpdateAccount(@Valid @RequestBody  CustomerDto customerDto){
+        boolean isUpdated = accountService.UpdateAccount(customerDto);
+        if(isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstant.STATUS_200, AccountsConstant.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AccountsConstant.STATUS_417, AccountsConstant.MESSAGE_417_UPDATE));
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto> DeleteAccount(@RequestParam
+                                                         @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+                                                         String mobileNumber){
+        boolean isDeleted = accountService.DeleteAccount(mobileNumber);
+        if(isDeleted) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstant.STATUS_200, AccountsConstant.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AccountsConstant.STATUS_417, AccountsConstant.MESSAGE_417_UPDATE));
+        }
     }
 
 
--- accounts/src/main/java/com/hp/accounts/Models/BaseEntity.java
+++ accounts/src/main/java/com/hp/accounts/Models/BaseEntity.java
@@ -2,23 +2,34 @@ package com.hp.accounts.Models;
 
 import jakarta.persistence.Column;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Getter @Setter @ToString
 public class BaseEntity {
 
+    @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
+
+    @CreatedBy
     @Column(updatable = false)
     private String createdBy;
-    @Column(updatable = false)
-    private LocalDateTime updateAt;
-    @Column(updatable = false)
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    @Column(insertable = false)
     private String updatedBy;
 }
